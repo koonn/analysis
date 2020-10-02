a491e625fef2273a92fded960ebcd082a8b0fd0e
Takahiro Konno <t.konno@shishimaro.co.jp>
2020-10-02 14:16:31 +0900

    :green_heart: テストの追加

---
+++ tests/test_util.py
@@ -0,0 +1,11 @@
+import pytest
+from src import Util
+
+
+def test_script_based_relative_path():
+    """Util.script_based_relative_pathの正常系テスト
+
+    TODO:
+        - 自分のローカルの絶対パスでテストしているので環境が変わったら書き換える
+    """
+    assert Util.convert_script_based_relative_path('../train.csv') == '/Users/takahirokonno/pyprojects/bunseki/train.csv'
