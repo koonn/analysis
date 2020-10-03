7b8b9a7ec8df4ce6ed277b44c766c188d3ca3011
Takahiro Konno <t.konno@shishimaro.co.jp>
2020-10-03 22:31:13 +0900

    :sparkles: 全指標を可視化するように修正

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,12 @@
+from itertools import product
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import precision_recall_curve, average_precision_score
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report, accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
 def show_all_metrics(y_true, y_pred_proba, threshold=0.5):
@@ -18,11 +21,15 @@ def show_all_metrics(y_true, y_pred_proba, threshold=0.5):
         None
     """
     # 先にグラフを描画するためにplt.showを使う
-    plot_roc_prc(y_true, y_pred_proba)
-    plt.show()
+    # 横並びのグラフ枠を作成
+    fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+    fig.subplots_adjust(wspace=0.4, hspace=0.6)
 
-    print_classification_report(y_true, y_pred_proba, threshold)
-    print_confusion_matrix(y_true, y_pred_proba, threshold)
+    # 指標を描画
+    plot_roc(y_true, y_pred_proba, ax=axes[0, 0])
+    plot_prc(y_true, y_pred_proba, ax=axes[0, 1])
+    plot_confusion_matrix(y_true, y_pred_proba, ax=axes[1, 0], threshold=threshold)
+    plot_classification_report(y_true, y_pred_proba, ax=axes[1, 1], threshold=threshold)
 
 
 def print_classification_report(y_true, y_pred_proba, threshold=0.5):
@@ -31,7 +38,7 @@ def print_classification_report(y_true, y_pred_proba, threshold=0.5):
     Args:
         y_true(1-D array-like shape of [n_samples, ]): 2値の目的ラベルの配列(ラベルは0または1)
         y_pred_proba(1-D array-like shape of [n_samples, ]): 陽性(ラベルが1)である確率の予測値の配列
-        threshold(float): 陽性と分類する確率の閾値. 陽性(ラベルが1)である確率の予測値がthreshold以上なら1に変換する. デフォルトでは0.5
+        threshold(float): 陽性と分類する確率の閾値. 陽性(ラベルが1)である確率の予測値がthreshold以上なら1に変換する. デフォルトでは0.None
 
     Returns:
         None
@@ -43,13 +50,95 @@ def print_classification_report(y_true, y_pred_proba, threshold=0.5):
     print(classification_report(y_true, y_pred_label))
 
 
+def plot_classification_report(y_true, y_pred_proba, threshold=0.5, ax=None):
+    """Precision, Recall, F1-Score等の指標を計算して表示する関数
+
+    Args:
+        y_true(1-D array-like shape of [n_samples, ]): 2値の目的ラベルの配列(ラベルは0または1)
+        y_pred_proba(1-D array-like shape of [n_samples, ]): 陽性(ラベルが1)である確率の予測値の配列
+        threshold(float): 陽性と分類する確率の閾値. 陽性(ラベルが1)である確率の予測値がthreshold以上なら1に変換する. デフォルトでは0.None
+
+    Args:
+        y_true:
+        y_pred_proba:
+        threshold:
+        ax (matplotlib axes, default=None): プロットするaxオブジェクト. Noneならば新しいfig, axを作成する
+
+    Returns:
+        None
+
+    """
+    # 予測確率を2値ラベルに変換する
+    y_pred_label = np.where(y_pred_proba >= threshold, 1, 0)
+
+    # 使用する指標を計算
+    precision, recall, fscore, support = precision_recall_fscore_support(y_true, y_pred_label, labels=[1, 0])
+    acc = accuracy_score(y_true, y_pred_label)
+
+    # precision, recall, fscoreをヒートマップ用の行列形式に整形する
+    matrix_measures = np.array([precision, recall, fscore]).T
+
+    # 描画の作成
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    else:
+        fig = ax.figure
+
+    # ヒートマップを作成
+    im_ = ax.imshow(matrix_measures, interpolation='nearest', cmap='Reds')
+
+    # テキストの色を背景色に合わせていい感じに調整するための閾値を作成
+    cmap_min, cmap_max = im_.cmap(0), im_.cmap(256)
+    color_threshold = (matrix_measures.max() + matrix_measures.min()) / 2.0
+
+    # matrix_measuresの行列数を取得(iterationで意味がわかりやすいようにするため、あえて名前をつけた)
+    n_classes = matrix_measures.shape[0]
+    n_measures = matrix_measures.shape[1]
+
+    # テキストを描画する
+    for i, j in product(range(n_classes), range(n_measures)):
+
+        # テキスト色を決める
+        if matrix_measures[i, j] < color_threshold:
+            text_color = cmap_max
+        else:
+            text_color = cmap_min
+
+        # テキストを描画
+        text = ax.text(x=j,
+                       y=i,
+                       s=f'{matrix_measures[i, j]:.2f}',
+                       ha='center',
+                       va='center',
+                       color=text_color,
+                       )
+
+    # そのほかの描画設定
+    fig.colorbar(im_, ax=ax)
+
+    ax.set_xticks(np.arange(3))
+    ax.set_xticklabels(['Precision', 'Recall', 'F1-score'])
+
+    ax.set_yticks(np.arange(2))
+    ax.set_yticklabels([f'1 \n (N={support[0]})',
+                        f'0 \n (N={support[1]})',
+                        ])  # supportのindexとラベルが逆なので注意
+
+    ax.set_xlabel('Measures')
+    ax.set_ylabel('True label')
+    ax.set_title(f'Classification report: Pos_decision_threshold={threshold} \n Accuracy={acc:.2f}')
+
+    fig.tight_layout()
+
+
 def print_confusion_matrix(y_true, y_pred_proba, threshold=0.5):
     """混合行列を整形して表示する関数
 
     Args:
         y_true(1-D array-like shape of [n_samples, ]): 2値の目的ラベルの配列(ラベルは0または1)
         y_pred_proba(1-D array-like shape of [n_samples, ]): 陽性(ラベルが1)である確率の予測値の配列
-        threshold(float, default=0.5): 陽性と分類する確率の閾値. 陽性(ラベルが1)である確率の予測値がthreshold以上なら1に変換する.
+        threshold(float, default=0.5): 陽性と分類する確率の閾値. 陽性(ラベルが1)である確率の予測値がthreshold以上なら1に変換する
+
     Returns:
         None
     """
@@ -67,23 +156,45 @@ def print_confusion_matrix(y_true, y_pred_proba, threshold=0.5):
     print(confusion_matrix_)
 
 
-def plot_roc_prc(y_true, y_pred_proba):
-    """ROC曲線とPR曲線をまとめて描画する関数
+def plot_confusion_matrix(y_true, y_pred_proba, threshold=0.5, ax=None):
+    """混合行列を整形してプロットとして表示する関数
 
     Args:
         y_true(1-D array-like shape of [n_samples, ]): 2値の目的ラベルの配列(ラベルは0または1)
         y_pred_proba(1-D array-like shape of [n_samples, ]): 陽性(ラベルが1)である確率の予測値の配列
+        threshold(float, default=0.5): 陽性と分類する確率の閾値. 陽性(ラベルが1)である確率の予測値がthreshold以上なら1に変換する
+        ax (matplotlib axes, default=None): プロットするaxオブジェクト. Noneならば新しいfig, axを作成する
 
     Returns:
         None
-
     """
-    # 横並びのグラフ枠を作成
-    fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+    # 予測確率を2値ラベルに変換する
+    y_pred_label = np.where(y_pred_proba >= threshold, 1, 0)
 
-    # rocとpr曲線を描画
-    plot_roc(y_true, y_pred_proba, ax=axes[0])
-    plot_prc(y_true, y_pred_proba, ax=axes[1])
+    # 混合行列の算出
+    confusion_matrix_ = confusion_matrix(y_true, y_pred_label, labels=[1, 0])
+
+    # 描画の作成
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+    else:
+        fig = ax.figure
+
+    # 混合行列のディスプレイ作成のためのConfusionMatrixDisplayインスタンス作成
+    disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix_,
+                                  display_labels=[1, 0]
+                                  )
+
+    # 描画の作成
+    disp.plot(
+        include_values=True,
+        cmap='Blues',
+        ax=ax,
+        xticks_rotation='horizontal',
+        values_format=None,
+    )
+
+    ax.set_title(f'Confusion Matrix: Pos_decision_threshold={threshold}')
 
 
 def plot_roc(y_true, y_pred_proba, ax=None):
