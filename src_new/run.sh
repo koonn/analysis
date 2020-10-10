7bfdd251a0031d5ce2229fa820fe1e05e492859d
Takahiro Konno <t.konno@shishimaro.co.jp>
2020-10-10 16:08:01 +0900

    :+1: xgboostの追加

--- src_new/run.sh
+++ src_new/run.sh
@@ -4,12 +4,13 @@
 SCRIPT_DIR="$(cd "$(dirname $0)"; pwd)"
 
 # 実行
-python "$SCRIPT_DIR/train.py" --fold 0 --model logistic_regression
-python "$SCRIPT_DIR/train.py" --fold 1 --model logistic_regression
-python "$SCRIPT_DIR/train.py" --fold 2 --model logistic_regression
-python "$SCRIPT_DIR/train.py" --fold 3 --model logistic_regression
-python "$SCRIPT_DIR/train.py" --fold 4 --model logistic_regression
+#python "$SCRIPT_DIR/train.py" --fold 0 --model logistic_regression
+#python "$SCRIPT_DIR/train.py" --fold 1 --model logistic_regression
+#python "$SCRIPT_DIR/train.py" --fold 2 --model logistic_regression
+#python "$SCRIPT_DIR/train.py" --fold 3 --model logistic_regression
+#python "$SCRIPT_DIR/train.py" --fold 4 --model logistic_regression
 
+python "$SCRIPT_DIR/train.py" --fold 0 --model xgboost
 
 # 警告を表示したくない時
 # python -W ignore train.py --fold 0 --model logistic_regression
